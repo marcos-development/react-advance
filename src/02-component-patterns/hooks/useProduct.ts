cd2ae64ba543ad1ec1cc6c24bc514cd188de1ece
marcos tataje <fernando.app.dev@gmail.com>
2021-12-02 13:45:41 +0100

    Control props alt

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Product, onChangeArgs } from '../interfaces/interfaces';
 
 interface useProductArgs {
@@ -11,16 +11,10 @@ const useProduct = ( { onChange, product, value = 0 }: useProductArgs ) => {
 
     const [counter, setCounter] = useState(value);
 
-    const isControlled = useRef( !!onChange );
 
     const increaseBy = (valueIncrease: number) => {
 
-        if ( isControlled.current ) {
-            return onChange!({ count: valueIncrease, product });
-        }
-
         const newValue = Math.max( counter + valueIncrease, 0 );
-
         setCounter( newValue );
 
         onChange && onChange({ count: newValue, product });
--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -9,30 +9,17 @@ export const useShoppingCart = () => {
 
         setShoppingCart( oldShopping => {
 
-            const productInCar: ShoppingCart = oldShopping[product.id] || { ...product, count: 0 };
-
-            if ( Math.max(productInCar.count + count, 0) > 0 ) {
-                productInCar.count += count;
+            if (count) {
                 return {
                     ...oldShopping,
-                    [product.id]: productInCar
+                    [product.id]: {...product, count}
                 }
+            } else {
+                // Object.keys(oldShopping)
+                //         .filter(key => key === product.id ? delete oldShopping[key] : null)
+                const { [product.id]: toDelete, ...rest } = oldShopping;
+                return { ...rest };
             }
-
-            const { [product.id]: toDelete, ...rest } = oldShopping;
-            return { ...rest };
-
-            // if (count) {
-            //     return {
-            //         ...oldShopping,
-            //         [product.id]: {...product, count}
-            //     }
-            // } else {
-            //     // Object.keys(oldShopping)
-            //     //         .filter(key => key === product.id ? delete oldShopping[key] : null)
-            //     const { [product.id]: toDelete, ...rest } = oldShopping;
-            //     return { ...rest };
-            // }
         })
         
     }
